2787cdb1a28b4471c95fdf43ee38684a8e2a4687
Baskey7261 <elbatsab77@gmail.com>
2021-12-10 15:34:19 +0000

    playpause toggle

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ function learnMore(x) {
     $(prev).slideToggle();
     x.innerText = prev.offsetHeight < 3 ? x.dataset['label2'] : x.dataset['label1'];
 }
+
+
+
+function audioToggle() {
+    let t = 0;
+
+    function out() {
+        x = document.getElementById("play");
+        t == 0 ? (x.play(), t++) : (x.pause(), t--);
+    }
+    return out;
+}
+let toggleAudio = audioToggle();
